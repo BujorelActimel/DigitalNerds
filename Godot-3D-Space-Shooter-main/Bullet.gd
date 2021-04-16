816ee4514180c50024c6ffdf33ecd502f88544ab
Scipex <67282743+Sc1pex@users.noreply.github.com>
2021-04-16 14:40:04 +0300

    Enemies shoot

--- Godot-3D-Space-Shooter-main/Bullet.gd
+++ Godot-3D-Space-Shooter-main/Bullet.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 var velo = Vector3()
+var playerBullet = true;
 var KillParticles = load("res://KillParticles.tscn")
 onready var main = get_tree().current_scene
 onready var explodeSound = $EnemyExplode
@@ -9,11 +10,19 @@ func _physics_process(delta):
 	move_and_slide(velo)
 
 func _on_Area_body_entered(body):
-	if body.is_in_group("Enemies"):
+	if body.is_in_group("Enemies") and playerBullet == true:
 		var particles = KillParticles.instance()
 		main.add_child(particles)
 		particles.transform.origin = transform.origin
 		body.queue_free()
+		explodeSound.play()
+		visible = false
+		$Area/CollisionShape.disabled = true
+	elif body.is_in_group("Player") and playerBullet == false:
+		var particles = KillParticles.instance()
+		main.add_child(particles)
+		particles.transform.origin = transform.origin
+		print("Player hit")
 		explodeSound.play()
 		visible = false
 		$Area/CollisionShape.disabled = true
